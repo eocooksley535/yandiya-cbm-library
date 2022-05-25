8855632cd6d4d8b28b86d756ccff34574aaa4cc4
tdgog <elvisoberoatkins.dev@gmail.com>
2022-05-25 16:33:29 +0100

    project scaffolding

---
+++ cbmcalculator.py
@@ -0,0 +1,15 @@
+"""
+Author: Elvis Obero-Atkins
+Last Edited by: Elvis Obero-Atkins
+
+This py script is main component of the yandiya-cbm-library
+As well as this py script all the csv files are required also.
+These includes: product-details.csv, pallet-types.csv, parcel-types.csv & delivery-costs.csv
+
+"""
+
+import csv
+
+def cbm_calculator():
+    return 0
+
---
+++ cli-interface.py
@@ -0,0 +1,16 @@
+"""
+Author: Elvis Obero-Atkins
+Last Edited by: Elvis Obero-Atkins
+
+This py script is NOT apart of the yandiya-cbm-library
+
+It is a simple CLI python application that allows the user to 
+interact with the yandiya-cbm-library.
+
+It was cretaed to make the inital developemnt of 
+the yandiya-cbm-library easier to test
+
+"""
+
+import cbmcalculator 
+
