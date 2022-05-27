79bac3ff0e2c8bac17dfee8fcfdbc0657775beca
elvis <elvisoberoatkins.dev@gmail.com>
2022-05-27 16:22:55 +0100

    adding dev reference files + modifying docString

--- cli_interface.py
+++ cli_interface.py
@@ -2,13 +2,16 @@
 Author: Elvis Obero-Atkins
 Last Edited by: Elvis Obero-Atkins
 
-This py script is NOT apart of the yandiya-cbm-library
+This py script is NOT REQUIRED for yandiya-cbm-library to function
 
 It is a simple CLI python application that allows the user to 
 interact with the yandiya-cbm-library.
 
 It was created to make the initial development of 
 the yandiya-cbm-library easier to test
+
+later versions of this repository will 
+include an Odoo module and WebUI to interact with the yandiya-cbm-library
 
 """
 import cbmcalculator
