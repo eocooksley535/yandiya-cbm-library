2c256177866753a32fb179bf2292eaedf6b55e9b
elvis <elvisoberoatkins.dev@gmail.com>
2022-05-31 16:24:26 +0100

    renamed files, added notes + started excel search

--- .excel_db_test/excel_out.py
+++ .excel_db_test/excel_out.py
@@ -2,43 +2,32 @@
 Author: Elvis Obero-Atkins
 Last Edited by: Elvis Obero-Atkins
 
-This py script is NOT REQUIRED for yandiya-cbm-library to function
-and will most likely be removed or archived in the final version
+This python script was created to test the viability of using excel instead of CSVs (at least for the MVP)
 
-It is a simple CLI python application that prints out the 
-values from an excel doc 
-
-It was created to test the viability for using an excel doc.
+This script allows the user to in put the amount of columns they would like to view by typing the letters of the columns.
 
 """
 
 import openpyxl
 
-theFile = openpyxl.load_workbook(
-    '.excel_db_test\product-details.xlsx')
-allSheetNames = theFile.sheetnames
+yandiya_db = openpyxl.load_workbook(
+    '.excel_db_test\yandiya-db.xlsx')
+all_sheets = yandiya_db.sheetnames
 
 # Default parameters for product-details.xlsx is "ABCDEFGHIJKLMNOPQ"
 
 parameters = str(input(
     "Please enter the the columns in alphabetical order that you wish to display "))
 
-print("All sheet names {} " .format(theFile.sheetnames))
+print("All sheet names {} " .format(yandiya_db.sheetnames))
 
 
-for sheet in allSheetNames:
+for sheet in all_sheets:
     print("Current sheet name is {}" .format(sheet))
-    currentSheet = theFile[sheet]
-    # print(currentSheet['B4'].value)
-
-    # print max numbers of wors and colums for each sheet
-    # print(currentSheet.max_row)
-    # print(currentSheet.max_column)
+    currentSheet = yandiya_db[sheet]
 
     for row in range(1, currentSheet.max_row + 1):
-        # print(row)
-        for column in parameters:  # Here you can add or reduce the columns
+        for column in parameters:
             cell_name = "{}{}".format(column, row)
-            # print(cell_name)
             print("cell position {} has value {}".format(
                 cell_name, currentSheet[cell_name].value))
---
+++ .excel_db_test/excel_search.py
@@ -0,0 +1,22 @@
+"""
+Author: Elvis Obero-Atkins
+Last Edited by: Elvis Obero-Atkins
+
+This python script was created to test the viability of using excel instead of CSVs (at least for the MVP)
+
+This script allows the user to input the partNo of the product they wish to get the infomation of.
+
+"""
+
+import openpyxl
+
+yandiya_db = openpyxl.load_workbook(
+    '.excel_db_test\yandiya-db.xlsx')
+
+# print(yandiya_db.sheetnames)
+
+records_table = yandiya_db.active
+
+# print(records_table)
+
+# ask user for input, iterate over column PartNo or "A" , check if input value is equal to any values in column
